bcc9c4a91d04690e28d3d0eb347b0ff0c1c89402
Jessica Gondim <jessicagbsg@gmail.com>
2023-07-09 16:24:31 -0300

    devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,9 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+  # Devise config
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded any time
@@ -40,6 +43,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
+  
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -67,4 +71,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
 end
